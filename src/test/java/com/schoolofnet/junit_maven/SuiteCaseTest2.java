84a2b3676ca862ebe21b8ae6c044c88c816b8ad8
SamuWesll <samuelwrochabarboza@gmail.com>
2021-10-01 19:17:07 -0300

    Rodando testes em ordem

--- src/test/java/com/schoolofnet/junit_maven/SuiteCaseTest2.java
+++ src/test/java/com/schoolofnet/junit_maven/SuiteCaseTest2.java
@@ -1,14 +1,24 @@
 package com.schoolofnet.junit_maven;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SuiteCaseTest2 {
 
     @Test
     public void sub() {
         assertThat(2 - 2, is(0));
     }
+
+    @Test
+    public void mult() {
+        assertThat(2 * 2, is(4));
+    }
+
 
 }
